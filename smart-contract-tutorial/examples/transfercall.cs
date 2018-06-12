72669ef876c152748df79b4ce2e1f3dbb1878f8c
chris <tcpdumpp@163.com>
2018-06-12 19:01:32 +0800

    ONT &ONG ABI change (#16)
    * add feemodel doc
    
    * add image link
    
    * Create transfercall.cs
    
    * Update transfercall.cs
    
    * add ontology gas fee model english markdown
    
    * Update feemodel_cn.md

---
+++ smart-contract-tutorial/examples/transfercall.cs
@@ -0,0 +1,30 @@
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace NativeContract
+{
+    public class Contract1 : SmartContract
+    {
+        struct State
+        {
+            public byte[] From;
+            public byte[] To;
+            public int Value;
+        }
+
+        public static Object Main()
+        {
+            int value = 1000;
+            byte[] address = { 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+            byte[] from = { 76, 50, 165, 99, 113, 232, 4, 127, 50, 221, 89, 105, 7, 158, 55, 156, 244, 10, 38, 198 };
+            byte[] to = { 76, 50, 165, 99, 113, 232, 4, 127, 50, 221, 89, 105, 7, 158, 55, 156, 244, 10, 38, 198 };
+
+            object[] param = new object[1];
+            param[0] = new State { From = from, To = to, Value = 10 };
+
+            return Native.Invoke(0, address, "transfer", param);
+        }
+    }
+}
